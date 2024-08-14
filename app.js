04916113c10c68d2faafb68c0fc401c9f9ae178e
Erdoğan Bülbül <167688600+erbul69@users.noreply.github.com>
2024-08-15 01:58:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 require("dotenv").config();
 let ipaddresses = "";
-let date_time = "";
+let update_time = "";
 
 const server = http.createServer(function(req, res){
     if(req.url == "/setip"){
         ipaddresses = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        date_time = new Date();
+        update_time = getDate();
         res.end();
     }
     else if(req.url == "/getip"){
     const htmlString = "<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>my home ip</title></head><body>" +
-        "<h2>Merhaba!</h2><p><b>Ip Adress: " + ip + "</b></p><p><b>Güncelleme:</b> " + getDate() + "</p></body></html>";
+        "<h2>Merhaba!</h2><p><b>Ip Adress: " + ip + "</b></p><p><b>Güncelleme:</b> " + update_time + "</p></body></html>";
         res.write(htmlString);
         res.end();
     }
@@ -24,6 +24,7 @@ const server = http.createServer(function(req, res){
 });
 
 function getDate(){
+    let date_time = new Date();
     let date = ("0" + date_time.getDate()).slice(-2);
     let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
     let year = date_time.getFullYear();
