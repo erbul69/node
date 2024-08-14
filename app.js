744e36f4aa5f3781990cae6dcdee9d64f9c36cc3
Erdoğan Bülbül <167688600+erbul69@users.noreply.github.com>
2024-08-14 21:38:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ let ipaddresses = [];
 app.use("/setip", function(req, res){
     ipaddresses = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     res.send("Ip adresiniz kaydedildi");
-]);
+});
 
 app.use("/getip", function(req, res){
     res.send("Merhaba! ip adresiniz : " + ipaddresses[0});
-]);
+});
 
 app.listen(PORT, () => {
     console.log(`Web Server ${PORT} numaralı port üzerinde çalışıyor`);
