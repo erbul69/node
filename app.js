7e3311d44345982a8249e82dd58f15599691e359
Erdoğan Bülbül <167688600+erbul69@users.noreply.github.com>
2024-08-15 00:03:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let ipaddresses = "";
 const server = http.createServer(function(req, res){
     if(req.url == "/setip"){
         ipaddresses = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log("Ip adresi günsellendi");
         res.write("Ip adresiniz kaydedildi");
         res.end();
     }
