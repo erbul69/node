2153a774d741bae9e23eb48e4152a2f829bb86bf
Erdoğan Bülbül <167688600+erbul69@users.noreply.github.com>
2024-08-14 23:42:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const PORT = process.env.PORT || 3000;
 require("dotenv").config();
-let ipaddresses;
+let ipaddresses = "";
 
 const server = http.createServer(function(req, res){
     if(req.url == "/setip"){
