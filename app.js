fc407b7018dec02bfe94c13a8d254b2220f2e265
Erdoğan Bülbül <167688600+erbul69@users.noreply.github.com>
2024-08-15 03:10:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const server = http.createServer(function(req, res){
         res.end();
     }
     else if(req.url == "/getip"){
-        let html = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>my home ip</title></head><body>";
-        html += "<h2>Merhaba!</h2><p><b>Ip Adress:</b> " + ipaddresses + "</p><p><b>Güncelleme:</b> " + update_time + "</p></body></html>";
+        let html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>my home ip</title></head><body>';
+        html += '<h2>Merhaba!</h2><p><b>Ip Adress:</b> ' + ipaddresses + '</p><p><b>Güncelleme:</b> ' + update_time + '</p></body></html>';
         res.writeHead(200, {'Content-Type': 'text/html'})
-        response.end(html);
+        res.end(html);
     }
     else{
         res.write("Merhaba Dünya!");
