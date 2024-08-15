970913e3121aa26f2bf67184f8a71d7ad1c28273
Erdoğan Bülbül <167688600+erbul69@users.noreply.github.com>
2024-08-15 17:29:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer(function(req, res){
                 let user = JSON.parse(data);
                 if(user.username == process.env.username && user.password == process.env.password) {
                     ipaddresses = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-                    update_time = getDate();
+                    update_time = Date();
                 }
             });
             req.on('end', function() {
@@ -24,8 +24,9 @@ const server = http.createServer(function(req, res){
     }
     else if(req.url == "/getip"){
         const ip = ipaddresses.split(",")[0];
+        const date = update_time.toLocaleString("tr-TR", { timeZone: "Asia/Istanbul" })
         let html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>my home ip</title></head><body>';
-        html += '<h2>Merhaba!</h2><p><b>Ip Adress:</b> ' + ip + '</p><p><b>Güncelleme:</b> ' + update_time + '</p></body></html>';
+        html += '<h2>Merhaba!</h2><p><b>Ip Adress:</b> ' + ip + '</p><p><b>Güncelleme:</b> ' + date + '</p></body></html>';
         
         res.writeHead(200, {'Content-Type': 'text/html'})
         res.end(html);
@@ -35,18 +36,6 @@ const server = http.createServer(function(req, res){
         res.end();
     }
 });
-
-function getDate(){
-    let date_time = new Date();
-    let date = ("0" + date_time.getDate()).slice(-2);
-    let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
-    let year = date_time.getFullYear();
-    let hours = date_time.getHours();
-    let minutes = date_time.getMinutes();
-    let seconds = date_time.getSeconds();
-    
-    return(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
-};
 
 server.listen(PORT, () => {
     console.log(`Web Server ${PORT} numaralı port üzerinde çalışıyor`);
