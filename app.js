240f1d444e917b8b996c4e8b53e8a6f00dd75740
Erdoğan Bülbül <167688600+erbul69@users.noreply.github.com>
2024-08-15 03:17:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ let ipaddresses = "";
 let update_time = "";
 
 const server = http.createServer(function(req, res){
+    /*if (req.method == 'POST') {
+        console.log('POST')
+        var body = ''
+        req.on('data', function(data) {
+            body += data
+            console.log('Partial body: ' + body)
+        })
+        req.on('end', function() {
+            console.log('Body: ' + body)
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end('post received')
+        })
+    }*/
     if(req.url == "/setip"){
         ipaddresses = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         update_time = getDate();
