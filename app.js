f0634d63675beda68fe5c1020ac3ae6c8fe83caf
Erdoğan Bülbül <167688600+erbul69@users.noreply.github.com>
2024-08-15 21:34:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const server = http.createServer(function(req, res){
         const ip = ipaddresses.split(",")[0];
         const data_time = update_time.toLocaleString("tr-TR", { timeZone: "Asia/Istanbul" });
         let html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>my home ip</title></head><body>';
-        html += '<h2>Merhaba!</h2><p><b>Ip Adress:</b> ' + ip + '</p><p><b>Güncelleme:</b><span id="date"> ' + '' + '</span></p></body>'<script>';
-        html += 'document.getElementById("date").innerText = "' + update_time.toLocaleString("tr-TR", {timeZone: "Asia/Istanbul"}) + '" + " (GMT+3)";</script></html>'
+        html += '<h2>Merhaba!</h2><p><b>Ip Adress:</b> ' + ip + '</p><p><b>Güncelleme:</b><span id="date"> ' + '' + '</span></p></body>';
+        html += '<script>document.getElementById("date").innerText = "' + update_time.toLocaleString("tr-TR", {timeZone: "Asia/Istanbul"}) + '" + " (GMT+3)";</script></html>'
         res.writeHead(200, {'Content-Type': 'text/html'})
         res.end(html);
     }
