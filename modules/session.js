2967f218af762c700a7ddf76b35cae5eaf1acc96
Erdoğan Bülbül <167688600+erbul69@users.noreply.github.com>
2024-08-29 21:22:23 +0300

    Update session.js

--- modules/session.js
+++ modules/session.js
@@ -1,10 +1,6 @@
 const session = require('express-session');
-const MemoryStore = require('memorystore')(session)
 
 const sessionMiddleware = session({
-    store: new MemoryStore({
-      checkPeriod: 86400000 // prune expired entries every 24h
-    }),
     secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true,
